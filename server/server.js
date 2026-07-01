98c099a241a548c1910f04d84129bb42c70f6c3e
madhumitharamkumarr <madhumitharamkumarr@gmail.com>
2026-07-01 23:29:34 +0530

    AWS Terraform Docker deployment setup

--- server/server.js
+++ server/server.js
@@ -18,7 +18,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://13.203.206.2:5173"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
